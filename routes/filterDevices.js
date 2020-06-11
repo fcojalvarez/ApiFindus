f511790a61de38ce5ff002df0aeae5705e25bca3
Francisco J. Álvarez <fcojalvarezrodriguez@gmail.com>
2020-06-11 20:22:59 +0200

    add filter from devices 50%

--- routes/filterDevices.js
+++ routes/filterDevices.js
@@ -9,26 +9,47 @@ app.use(json());
 
 
 router.route('/filterDevices')
-    .get(async(req, res) => {
+    .post(async(req, res) => {
         try {
             let devicesList = await Device.find().exec();
+            console.log(req.body)
 
             let dataForm = {
                 almacenamiento: req.body.almacenamiento,
                 camaraFrontal: req.body.camaraFrontal,
-                camaraTrasera: req.body.camaraTrasea,
+                camaraTrasera: req.body.camaraTrasera,
                 memoriaRam: req.body.memoriaRam,
                 pantalla: req.body.pantalla,
                 precio: req.body.precio,
                 sistemaOperativo: req.body.sistemaOperativo
             }
 
-            devicesList.filter(device => device.price < dataForm.precio)
+            // Filtro rango precio
+            devicesList = devicesList.filter(device => device.price < dataForm.precio)
+
+            // Filtro Sistema Operativo
+            if (dataForm.sistemaOperativo === 'Android') {
+                devicesList = devicesList.filter(device => device.os[0].split(' ')[0] === 'Android')
+            } else if (dataForm.sistemaOperativo === 'IOS') {
+                devicesList = devicesList.filter(device => device.os[0].split(' ')[0] === 'iOS')
+            }
+
+            // Filtro tamaño pantalla
+            if (dataForm.pantalla === "Menos de 6''") {
+                devicesList = devicesList.filter(device => device.display[1] < 6)
+            } else if (dataForm.pantalla === "Más de 6''") {
+                devicesList = devicesList.filter(device => device.display[1] >= 6)
+            }
+
+            // Filtro memoria RAM (NO PUEDO ACCEDER A STORAGE)
+
+            // Filtro almacenamiento (NO PUEDO ACCEDER A STORAGE)
+
 
 
             res.json(devicesList);
         } catch (err) {
-            console.log(err)
+            res.status(404).json({ message: err.message })
         }
     })
 
